a4408c493d9805317390d639e941615919588c05
NCVillani <NCVillani@yahoo.com>
2013-07-11 20:24:59 -0600

    Stuck on what to do with paintComponent? should we call if from the action Preformed. If so how do we do it? what object is passed into the paintComponent method when its called.

--- src/edu/weber/cs3230/project3/RandomRectangles.java
+++ src/edu/weber/cs3230/project3/RandomRectangles.java
@@ -1,8 +1,13 @@
 package edu.weber.cs3230.project3;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -25,26 +30,67 @@ public class RandomRectangles {
 }
 
 class RectangleFrame extends JFrame {
+	
+   private JPanel buttonPanel;
+   private JPanel mainPanel;
+   private JButton moreButton;
+   private JButton fewerButton; 
+   public static final int DEFAULT_WIDTH = 500;
+   public static final int DEFAULT_HEIGHT = 700;
+   private int numRectangles;
+	
 	public RectangleFrame() {
-		setSize(300,400);
-		JButton moreButton = new JButton("More");
-		JButton fewerButton = new JButton("Fewer");
+		setTitle("Random Rectangle Viewer");
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		moreButton = new JButton("More");
+		fewerButton = new JButton("Fewer");
+		numRectangles = 100;
 		
 		buttonPanel = new JPanel();
 		mainPanel = new JPanel();
 		
+		buttonPanel.add(moreButton);
+		buttonPanel.add(fewerButton);
+		
+		add(mainPanel);
+		add(buttonPanel, BorderLayout.SOUTH);
+		
+		
+		
+		moreButton.addActionListener(new RectangleAction());
+		fewerButton.addActionListener(new RectangleAction());
+		
+		
 	}
-	public void paintComponent(Graphics g)
+	   private class RectangleAction implements ActionListener
 	   {
-	      Graphics2D g2 = (Graphics2D) g;
-	
-	   
+	      //this is called when the actionlistener is notified that what it is listening for happened. it than calls the action Performed
+	      public void actionPerformed(ActionEvent event)
+	      {
+	         //mainPanel.setBackground(Color.GREEN);
+	            	  
+	    	  
+	         if (event.getSource() == moreButton) {
+	        	 numRectangles *= 2;
+	        	 //paintComponent(this.getGraphics());
+	         } else if (event.getSource() == fewerButton) {
+	        	 numRectangles /= 2;
+	         }
+	      }
+	      
+		  public void paintComponent(Graphics g)
+		   {
+		      Graphics2D g2 = (Graphics2D) g;
+		      Random r = new Random();
+		      int y = r.nextInt(DEFAULT_WIDTH) + 1;
+		      int x = r.nextInt(DEFAULT_HEIGHT) + 1;
+		      
+		   }	
 	   }
 	
-	   private JPanel buttonPanel;
-	   private JPanel mainPanel;
-	   public static final int DEFAULT_WIDTH = 400;
-	   public static final int DEFAULT_HEIGHT = 500;
-	   public static final int DEFAULT_HIEGHT_BUTTONPANEL2 = 100;
-	
 }
+
+
+
+
+

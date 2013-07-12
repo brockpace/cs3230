d99742a137d3ee78bb63f453b343656d988094b9
NCVillani <NCVillani@yahoo.com>
2013-07-11 19:52:44 -0600

    2nd update start of adding buttons

--- src/edu/weber/cs3230/project3/RandomRectangles.java
+++ src/edu/weber/cs3230/project3/RandomRectangles.java
@@ -1,8 +1,12 @@
 package edu.weber.cs3230.project3;
 
 import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class RandomRectangles {
 
@@ -21,7 +25,26 @@ public class RandomRectangles {
 }
 
 class RectangleFrame extends JFrame {
+	public RectangleFrame() {
+		setSize(300,400);
+		JButton moreButton = new JButton("More");
+		JButton fewerButton = new JButton("Fewer");
+		
+		buttonPanel = new JPanel();
+		mainPanel = new JPanel();
+		
+	}
+	public void paintComponent(Graphics g)
+	   {
+	      Graphics2D g2 = (Graphics2D) g;
 	
+	   
+	   }
 	
+	   private JPanel buttonPanel;
+	   private JPanel mainPanel;
+	   public static final int DEFAULT_WIDTH = 400;
+	   public static final int DEFAULT_HEIGHT = 500;
+	   public static final int DEFAULT_HIEGHT_BUTTONPANEL2 = 100;
 	
 }
